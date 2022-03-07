6ef82fe4a29d03ec718b52ed243419951fda9602
Mykola Veremchuk <veremchukmukola@gmail.com>
2022-03-07 12:36:34 +0200

    added filter by message.text length

--- src/alert_messages_filter.py
+++ src/alert_messages_filter.py
@@ -12,18 +12,29 @@ log = logging.getLogger(__name__)
 
 class AlertMessagesFilter:
     def __init__(
-        self, clue_phrases_for_filtering: List[str] = CLUE_PHRASES_FOR_FILTERING
+        self,
+        clue_phrases_for_filtering: List[str] = CLUE_PHRASES_FOR_FILTERING,
+        message_len_limit: int = 350,
     ):
         self._clue_phrases_for_filtering = clue_phrases_for_filtering
+        self._message_len_limit = message_len_limit
 
     def filter_messages(self, messages: List[Message]) -> List[Message]:
         alert_messages = [
-            message
-            for message in messages
-            if self._message_contains_filter_phrase(message)
+            message for message in messages if self._is_alert_message(message)
         ]
         log.info(f"Found {len(alert_messages)} messages with clue alert phrases")
         return alert_messages
+
+    def _is_alert_message(self, message: Message) -> bool:
+        return self._is_message_not_too_long(
+            message
+        ) and self._message_contains_filter_phrase(message)
+
+    def _is_message_not_too_long(self, message: Message) -> bool:
+        if message.text is None:
+            return False
+        return len(message.text) < self._message_len_limit
 
     def _message_contains_filter_phrase(self, message: Message) -> bool:
         if message.text is None:
--- src/tests/alert_messages_filter_test.py
+++ src/tests/alert_messages_filter_test.py
@@ -9,7 +9,7 @@ def alert_messages_filter():
     return AlertMessagesFilter()
 
 
-def test_if_alert_messages_gets_filtered(alert_messages_filter):
+def test_if_alert_messages_gets_filtered_by_phrases(alert_messages_filter):
     messages = [
         Message(id="1", text="asdfkasd;kf;lasdkfПоВіТрЯнА ТрИвоГаdfkasdjfjkasdhjfkjl"),
         Message(id="2", text="фів"),
@@ -19,3 +19,18 @@ def test_if_alert_messages_gets_filtered(alert_messages_filter):
     assert len(filtered_messages) == 2 and {
         message.id for message in filtered_messages
     } == {"1", "3"}
+
+
+def test_if_too_long_messages_gets_filtered(alert_messages_filter):
+    messages = [
+        Message(
+            id="3",
+            text="Відбій Повітряної Тривоги Повітряної Тривоги Повітряної Тривоги ПоВіТрЯнА ТрИвоГ"
+                 "аПоВіТрЯнА ТрИвоГаПоВіТрЯнА ТрИвоГаПоВіТрЯнА ТрИвоГаПоВіТрЯнА ТрИвоГаПоВіТрЯнА ТрИвоГа"
+                 "аПоВіТрЯнА ТрИвоГаПоВіТрЯнА ТрИвоГаПоВіТрЯнА ТрИвоГаПоВіТрЯнА ТрИвоГаПоВіТрЯнА ТрИвоГа"
+                 "аПоВіТрЯнА ТрИвоГаПоВіТрЯнА ТрИвоГаПоВіТрЯнА ТрИвоГаПоВіТрЯнА ТрИвоГаПоВіТрЯнА ТрИвоГа"
+                 "аПоВіТрЯнА ТрИвоГаПоВіТрЯнА ТрИвоГаПоВіТрЯнА ТрИвоГаПоВіТрЯнА ТрИвоГаПоВіТрЯнА ТрИвоГа",
+        ),
+    ]
+    filtered_messages = alert_messages_filter.filter_messages(messages)
+    assert len(filtered_messages) == 0
